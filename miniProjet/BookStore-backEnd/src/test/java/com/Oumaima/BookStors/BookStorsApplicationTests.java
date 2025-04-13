ce47f34b9960bac1764dfe6c4f45b95705b3ca51
Oumaima <omaimalaghmich16@gmail.com>
2025-04-13 20:18:54 +0100

    myMiniPorjet

---
+++ miniProjet/BookStore-backEnd/src/test/java/com/Oumaima/BookStors/BookStorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oumaima.BookStors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
